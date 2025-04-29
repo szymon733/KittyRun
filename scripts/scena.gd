86a8cc1722fbdc4fbbfde1cd2c79b221f7507842
Weronika Bonczał <257780@edu.p.lodz.pl>
2025-04-29 15:35:20 +0200

    obstacles

--- scripts/scena.gd
+++ scripts/scena.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+var bomb_scene = preload("res://sceny/bomb.tscn")
+var dog_scene = preload("res://sceny/dog.tscn")
+var trashcan_scene = preload("res://sceny/trashcan.tscn")
+var obstacle_types := [bomb_scene, dog_scene, trashcan_scene]
+var obstacles : Array
+
+
+
+
 const KITTY_START_POS := Vector2i(156,-33)
 const CAM_START_POS := Vector2i(470,-226)
 var score : int
@@ -8,14 +17,17 @@ var speed : float
 const START_SPEED : float = 10.0
 const MAX_SPEED : int =25
 var screen_size : Vector2i
+var ground_height : int
 var game_running : bool
+var last_obs
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 func new_game():
-	score = 0
+	score = 0 
 	show_score()
 	$Kitty.position = KITTY_START_POS
 	$Kitty.velocity = Vector2i(0, 0)
@@ -29,6 +41,10 @@ func _process(delta):
 		speed = START_SPEED + score / 5000
 		if speed > MAX_SPEED:
 			speed = MAX_SPEED
+		generate_obs()
+		
+		
+		
 		
 		$Kitty.position.x += speed
 		$Camera2D.position.x += speed
@@ -42,6 +58,26 @@ func _process(delta):
 		if Input.is_action_pressed("ui_accept"):
 			game_running = true
 			$SCORE.get_node("StartLabel").hide()
+
+
+func generate_obs():
+	if obstacles.is_empty() or last_obs.position.x < score + randi_range(300,500):
+		var obs_type = obstacle_types[randi() % obstacle_types.size()]
+		var obs
+		var max_obs = 3
+		for i in range(randi() % max_obs + 1):
+			obs = obs_type.instantiate()
+			var obs_height = obs.get_node("Sprite2D").texture.get_height()
+			var obs_scale = obs.get_node("Sprite2D").scale
+			var obs_x : int = screen_size.x + score + 100 + (i * 100)
+			var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y / 2) + 100
+			last_obs = obs
+			add_obs(obs, obs_x, obs_y)
+
+func add_obs(obs, x, y):
+	obs.position = Vector2i(x,y)
+	add_child(obs)
+	obstacles.append(obs)
 
 func show_score():
 	$SCORE.get_node("Score Label").text = "SCORE: " + str(score/SCORE_MODIFIER)
